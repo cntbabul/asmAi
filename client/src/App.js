e945b122fc1b7510e3676610591bfa7769afc3a1
Cntbabul <cntbabul@gmail.com>
2024-07-26 15:45:12 +0530

    2nd commit

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import styled, {ThemeProvider} from "styled-components";
+import {darkTheme} from "./utils/Theme";
+import Home from "./pages/Home.jsx";
+import CreatePost from "./pages/CreatePost";
+import { BrowserRouter, Route,  Routes } from "react-router-dom";
+
+
+const Container = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+background: ${({ theme }) => theme.bg};
+color: ${({ theme }) => theme.text_primary};
+overflow: hidden;
+transition: all 0.2s ease;
+`;
+
+const Wrapper = styled.div`
+position: relative;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+height: 100%;
+flex: 3;
+`;
+
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper >
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Home />} />
+</Routes>
+</BrowserRouter>
+
+     
+        
+        </Wrapper>
+      </Container>
+
+    </ThemeProvider>
+  );
+}
+
+export default App;
